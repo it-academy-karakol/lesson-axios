a33c827271e5c6e7130cbc7223af879b1fa5db07
Elaman Imashov <elaman@outlook.com>
2020-05-01 11:33:52 +0600

    Correct axios base url

--- src/axios.js
+++ src/axios.js
@@ -1,23 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create();
 
-instance.interceptors.request.use(request => {
-  console.log('Request successfully made');
-  return request;
-}, error => {
-  console.log('Request failed');
-  return new Promise(error);
-});
+instance.interceptors.request.use(
+  (request) => {
+    console.log("Request successfully made");
+    return request;
+  },
+  (error) => {
+    console.log("Request failed");
+    return new Promise(error);
+  }
+);
 
-instance.interceptors.response.use(response => {
-  console.log('Response successfully recieved');
-  return response;
-}, error => {
-  console.log('Response failed');
-  return new Promise(error);
-});
+instance.interceptors.response.use(
+  (response) => {
+    console.log("Response successfully recieved");
+    return response;
+  },
+  (error) => {
+    console.log("Response failed");
+    return new Promise(error);
+  }
+);
 
-instance.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+instance.defaults.baseURL = "https://blog-e93ec.firebaseio.com";
 
 export default instance;

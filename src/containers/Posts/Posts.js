42baea10328675021aded82e1c3cc72a54e06c45
Elaman Imashov <elaman@outlook.com>
2020-05-01 11:34:10 +0600

    Correct posts component to use firebase

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -3,32 +3,27 @@ import axios from "../../axios";
 import Post from "../../components/Post/Post";
 
 export default () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
-      .get("/posts")
-      .then((response) => {
-        // Get only first 8 posts
-        const data = response.data.slice(0, 6);
-
-        // Add fake author field
-        data.forEach((post) => (post.author = "Bakyt"));
-
-        setPosts(data);
-      })
-      .catch((error) => {
-        setError(true);
-      });
+      .get("/posts.json")
+      .then((response) => setPosts(response.data))
+      .catch((error) => setError(true));
   }, []);
 
   let postsOutput = <p>Loading...</p>;
+  if (posts) {
+    postsOutput = Object.keys(posts).map((id) => (
+      <Post key={id} {...posts[id]} />
+    ));
+  }
+  if (posts === null) {
+    postsOutput = <p>No blog posts found!</p>;
+  }
   if (error) {
     postsOutput = <p className="error">Error loading posts from server!</p>;
-  }
-  if (posts.length) {
-    postsOutput = posts.map((post) => <Post key={post.id} {...post} />);
   }
 
   return <div className="Posts">{postsOutput}</div>;

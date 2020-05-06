d1644ff27ab91489f48c23707e0441d2caa4a9c6
Elaman Imashov <elaman@outlook.com>
2020-05-06 09:21:40 +0600

    Remove interceptors remove global config in favor of instance

--- src/axios.js
+++ src/axios.js
@@ -1,29 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create();
-
-instance.interceptors.request.use(
-  (request) => {
-    console.log("Request successfully made");
-    return request;
-  },
-  (error) => {
-    console.log("Request failed");
-    return new Promise(error);
-  }
-);
-
-instance.interceptors.response.use(
-  (response) => {
-    console.log("Response successfully recieved");
-    return response;
-  },
-  (error) => {
-    console.log("Response failed");
-    return new Promise(error);
-  }
-);
-
 instance.defaults.baseURL = "https://blog-e93ec.firebaseio.com";
 
 export default instance;
